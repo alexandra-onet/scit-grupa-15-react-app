78c507a5c0a1b8e92dccc5acc42b6ec66d175243
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-09-13 09:29:58 +0300

    api fetch example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+// check the console for results
+export class App extends React.Component {
+  handleGetAllMovies = () => {
+    fetch("https://movies-app-siit.herokuapp.com/movies?take=99999999")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  handleGetAllMoviesByTitleAndYear = () => {
+    fetch("https://movies-app-siit.herokuapp.com/movies?Title=man&Year=2016")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  handleLogin = () => {
+    fetch("https://movies-app-siit.herokuapp.com/auth/login", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "mouse",
+        password: "cat",
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        localStorage.setItem("accessToken", json.accessToken);
+      });
+  };
+
+  handleLogOut = () => {
+    fetch("https://movies-app-siit.herokuapp.com/auth/logout", {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": localStorage.getItem("accessToken"),
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  handleAddMovie = () => {
+    fetch("https://movies-app-siit.herokuapp.com/movies", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Auth-Token": localStorage.getItem("accessToken"),
+      },
+      body: JSON.stringify({
+        Title: "Stefan cel Mare",
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleGetAllMovies}>Get all movies</button>
+        <button onClick={this.handleGetAllMoviesByTitleAndYear}>
+          Get movies by Title and Year
+        </button>
+        <button onClick={this.handleLogin}>Login</button>
+        <button onClick={this.handleLogOut}>Logout</button>
+        <button onClick={this.handleAddMovie}>Add Movie</button>
+      </div>
+    );
+  }
 }
 
 export default App;
